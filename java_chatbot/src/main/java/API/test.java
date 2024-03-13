d71cb9edc218bf90d69640320dba800ac8dcbd02
Todin <112660117+Todin13@users.noreply.github.com>
2024-03-13 22:53:55 +0100

    test.java work

--- java_chatbot/src/main/java/API/test.java
+++ java_chatbot/src/main/java/API/test.java
@@ -6,11 +6,15 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class test {
-    public static Map<String, Map<String, Object>> getWeatherForecast(String latitude, String longitude) {
-        Map<String, Map<String, Object>> hourlyWeatherData = new HashMap<>(); // Map to store hourly weather data
+    public static Map<String, Map<String, String>> getWeatherForecast(String latitude, String longitude) {
+
+        Map<String, Map<String, String>> goodMap = new HashMap<>();
+        Map<String, List<String>> alldata = new HashMap<>();
 
         try {
             URL weatherURL = new URL("https://api.open-meteo.com/v1/forecast?latitude=" + latitude +
@@ -33,55 +37,47 @@ public class test {
             String jsonResponse = weatherResponse.toString();
             // Assuming the JSON structure contains an array of hourly data under the "hourly" key
             int startIndex = jsonResponse.indexOf("\"hourly\":") + "\"hourly\":".length();
-            int endIndex = jsonResponse.indexOf("]", startIndex) + 1;
+            int endIndex = jsonResponse.indexOf("]}", startIndex) + 1;
             String hourlyData = jsonResponse.substring(startIndex, endIndex);
-
             // Split hourly data into individual hourly data objects
-            String[] hourlyDataArray = hourlyData.split("\\},\\{");
+            String[] hourlyDataArray = hourlyData.split("\\],");
 
-            // Process each hour's data
-            for (String hourData : hourlyDataArray) {
-                Map<String, Object> hourWeatherData = new HashMap<>(); // Map to store weather data for one hour
-
-                // Extract specific fields for this hour and add to the map
-                hourWeatherData.put("temperature_2m", extractFieldValue(hourData, "temperature_2m"));
-                hourWeatherData.put("relative_humidity_2m", extractFieldValue(hourData, "relative_humidity_2m"));
-                hourWeatherData.put("apparent_temperature", extractFieldValue(hourData, "apparent_temperature"));
-                hourWeatherData.put("precipitation_probability", extractFieldValue(hourData, "precipitation_probability"));
-                hourWeatherData.put("precipitation", extractFieldValue(hourData, "precipitation"));
-                hourWeatherData.put("rain", extractFieldValue(hourData, "rain"));
-                hourWeatherData.put("showers", extractFieldValue(hourData, "showers"));
-                hourWeatherData.put("snowfall", extractFieldValue(hourData, "snowfall"));
-                hourWeatherData.put("snow_depth", extractFieldValue(hourData, "snow_depth"));
-                hourWeatherData.put("weather_code", extractFieldValue(hourData, "weather_code"));
-                hourWeatherData.put("surface_pressure", extractFieldValue(hourData, "surface_pressure"));
-                hourWeatherData.put("cloud_cover", extractFieldValue(hourData, "cloud_cover"));
-                hourWeatherData.put("cloud_cover_low", extractFieldValue(hourData, "cloud_cover_low"));
-                hourWeatherData.put("cloud_cover_mid", extractFieldValue(hourData, "cloud_cover_mid"));
-                hourWeatherData.put("cloud_cover_high", extractFieldValue(hourData, "cloud_cover_high"));
-                // Add more fields as needed...
-
-                // Extract time for this hour and use it as the key in the outer map
-                String time = extractFieldValue(hourData, "time");
-                hourlyWeatherData.put(time, hourWeatherData);
+            for (String element : hourlyDataArray) {
+                // Splitting the string to extract the list of integers
+                String[] parts = element.split(":\\[");
+                String listName = parts[0].replaceAll("[\"{}]", "");
+                String data = parts[1].replaceAll("[\\[\\]\"]", ""); 
+                String[] numbers = data.split(",");
+                List<String> numberList = new ArrayList<>();
+                for (String number : numbers) {
+                    numberList.add(number);
+                }
+                alldata.put(listName, numberList);
             }
 
             weatherConnection.disconnect();
+
+            List<String> time = alldata.get("time");
+
+            // Loop over time data to construct the goodMap
+            for (int i = 0; i < time.size(); i++) {
+                Map<String, String> timeMap = new HashMap<>();
+                for (Map.Entry<String, List<String>> entry : alldata.entrySet()) {
+                    String key = entry.getKey();
+                    List<String> value = entry.getValue();
+
+                    // Skip time data
+                    if (!key.equals("time")) {
+                        timeMap.put(key, value.get(i));
+                    }
+                }
+                goodMap.put(time.get(i), timeMap);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return hourlyWeatherData;
-    }
-
-    // Helper method to extract field value from JSON object string
-    private static String extractFieldValue(String jsonObjectString, String fieldName) {
-        int fieldIndex = jsonObjectString.indexOf("\"" + fieldName + "\":");
-        if (fieldIndex != -1) {
-            int valueStartIndex = jsonObjectString.indexOf("\"", fieldIndex + fieldName.length() + 3) + 1;
-            int valueEndIndex = jsonObjectString.indexOf("\"", valueStartIndex);
-            return jsonObjectString.substring(valueStartIndex, valueEndIndex);
-        }
-        return null; // Field not found
+        return goodMap;
     }
 
     public static void main(String[] args) {
@@ -90,27 +86,27 @@ public class test {
         String latitude = ipAndLocation[2];
         String longitude = ipAndLocation[3];
     
-        Map<String, Map<String, Object>> weatherForecast = getWeatherForecast(latitude, longitude);
+        Map<String, Map<String, String>> weatherForecast = getWeatherForecast(latitude, longitude);
     
         printHourlyForecast(weatherForecast);
     }
     
-    private static void printHourlyForecast(Map<String, Map<String, Object>> weatherForecast) {
+    private static void printHourlyForecast(Map<String, Map<String, String>> weatherForecast) {
         // Print the header
         System.out.println("Hourly Forecast:");
         
         // Iterate over each time slot
-        for (Map.Entry<String, Map<String, Object>> entry : weatherForecast.entrySet()) {
+        for (Map.Entry<String, Map<String, String>> entry : weatherForecast.entrySet()) {
             String time = entry.getKey();
-            Map<String, Object> hourlyData = entry.getValue();
+            Map<String, String> hourlyData = entry.getValue();
     
             // Print time
             System.out.println("Time: " + time);
     
             // Iterate over each key-value pair in hourly data
-            for (Map.Entry<String, Object> hourEntry : hourlyData.entrySet()) {
+            for (Map.Entry<String, String> hourEntry : hourlyData.entrySet()) {
                 String key = hourEntry.getKey();
-                Object value = hourEntry.getValue();
+                String value = hourEntry.getValue();
     
                 // Print key-value pair
                 System.out.printf("%-20s %-25s\n", key, value);
