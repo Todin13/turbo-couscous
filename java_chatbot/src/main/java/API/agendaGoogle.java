b24925e5b8f04492b1fda507c96d4e12ab26eae7
Todin <112660117+Todin13@users.noreply.github.com>
2024-03-21 16:29:56 +0100

    Update agendaGoogle.java
    split deja mieux mais ne recupère que un evenement le plus ancien crée pour ceux a venir

--- java_chatbot/src/main/java/API/agendaGoogle.java
+++ java_chatbot/src/main/java/API/agendaGoogle.java
@@ -147,8 +147,6 @@ public class agendaGoogle {
 
     private static Map<String, Object> parseEvents(String jsonResponse) {
 
-        System.out.println(jsonResponse);
-
         Map<String, Object> jsonData = new HashMap<>();
         Map<String, String> jsonDefaultReminders = new HashMap<>();
         Map<String, Object> jsonItems = new HashMap<>();
@@ -158,160 +156,150 @@ public class agendaGoogle {
         Map<String, String> jsonEnd= new HashMap<>();
         Map<String, String> jsonReminders= new HashMap<>();
 
-        if(jsonResponse.contains("\"kind\":")){
-            String kind = jsonResponse.split("\"kind\":")[1].split("\"")[1];
-            jsonItems.put("kind",kind);
+        String kind = jsonResponse.split("\"kind\":")[1].split("\"")[1];
+        jsonData.put("kind",kind);
+        
+        String etag = jsonResponse.split("\"etag\":")[1].split("\"")[1];
+        jsonData.put("etag",etag);
+        
+        String summary = jsonResponse.split("\"summary\":")[1].split("\"")[1];
+        jsonData.put("summary",summary);
+        
+        String description = jsonResponse.split("\"description\":")[1].split("\"")[1];
+        jsonData.put("description",description);
+        
+        String updated = jsonResponse.split("\"updated\":")[1].split("\"")[1];
+        jsonData.put("updated",updated);
+        
+        String timeZone = jsonResponse.split("\"timeZone\":")[1].split("\"")[1];
+        jsonData.put("timeZone",timeZone);
+        
+        String accessRole = jsonResponse.split("\"accessRole\":")[1].split("\"")[1];
+        jsonData.put("accessRole",accessRole);
+        
+        
+        String defaultReminders = jsonResponse.split("\"defaultReminders\":")[1];
+        if(defaultReminders.contains("\"method\":")){
+            String method = defaultReminders.split("\"method\":")[1].split("\"")[1];
+            jsonDefaultReminders.put("method",method);
         }
-        if(jsonResponse.contains("\"etag\":")){
-            String etag = jsonResponse.split("\"etag\":")[1].split("\"")[1];
-            jsonItems.put("etag",etag);
+        if(defaultReminders.contains("\"minutes\":")){
+            String minutes = defaultReminders.split("\"minutes\":")[1].split("\"")[1];
+            jsonDefaultReminders.put("minutes",minutes);
         }
-        if(jsonResponse.contains("\"summary\":")){
-            String summary = jsonResponse.split("\"summary\":")[1].split("\"")[1];
-            jsonItems.put("summary",summary);
+        jsonData.put("defaultReminders", jsonDefaultReminders);
+        
+        String nextSyncToken = jsonResponse.split("\"nextSyncToken\":")[1].split("\"")[1];
+        jsonData.put("nextSyncToken",nextSyncToken);
+        
+        
+        String items = jsonResponse.split("\"items\":")[1];
+        if(items.contains("\"kind\":")){
+            String kindItems = items.split("\"kind\":")[1].split("\"")[1];
+            jsonItems.put("kind",kindItems);
         }
-        if(jsonResponse.contains("\"description\":")){
-            String description = jsonResponse.split("\"description\":")[1].split("\"")[1];
-            jsonItems.put("description",description);
+        if(items.contains("\"etag\":")){
+            String etagItems = items.split("\"etag\":")[1].split("\"")[1];
+            jsonItems.put("etag",etagItems);
         }
-        if(jsonResponse.contains("\"updated\":")){
-            String updated = jsonResponse.split("\"updated\":")[1].split("\"")[1];
-            jsonItems.put("updated",updated);
+        if(items.contains("\"id\":")){
+            String id = items.split("\"id\":")[1].split("\"")[1];
+            jsonItems.put("id",id);
         }
-        if(jsonResponse.contains("\"timeZone\":")){
-            String timeZone = jsonResponse.split("\"timeZone\":")[1].split("\"")[1];
-            jsonItems.put("timeZone",timeZone);
+        if(items.contains("\"status\":")){
+            String status = items.split("\"status\":")[1].split("\"")[1];
+            jsonItems.put("status",status);
         }
-        if(jsonResponse.contains("\"accessRole\":")){
-            String accessRole = jsonResponse.split("\"accessRole\":")[1].split("\"")[1];
-            jsonItems.put("accessRole",accessRole);
+        if(items.contains("\"htmlLink\":")){
+            String htmlLink = items.split("\"htmlLink\":")[1].split("\"")[1];
+            jsonItems.put("htmlLink",htmlLink);
         }
-        if(jsonResponse.contains("\"defaultReminders\":")){
-            String defaultReminders = jsonResponse.split("\"defaultReminders\":")[1].split("\"")[1];
-            if(defaultReminders.contains("\"method\":")){
-                String method = defaultReminders.split("\"method\":")[1].split("\"")[1];
-                jsonDefaultReminders.put("method",method);
-            }
-            if(defaultReminders.contains("\"minutes\":")){
-                String minutes = defaultReminders.split("\"minutes\":")[1].split("\"")[1];
-                jsonDefaultReminders.put("minutes",minutes);
-            }
-            jsonData.put("defaultReminders", jsonDefaultReminders);
+        if(items.contains("\"created\":")){
+            String created = items.split("\"created\":")[1].split("\"")[1];
+            jsonItems.put("created",created);
         }
-        if(jsonResponse.contains("\"nextSyncToken\":")){
-            String nextSyncToken = jsonResponse.split("\"nextSyncToken\":")[1].split("\"")[1];
-            jsonItems.put("nextSyncToken",nextSyncToken);
+        if(items.contains("\"updated\":")){
+            String updatedItems = items.split("\"updated\":")[1].split("\"")[1];
+            jsonItems.put("updated",updatedItems);
         }
-        if(jsonResponse.contains("\"items\":")){
-            String items = jsonResponse.split("\"items\":")[1].split("\"")[1];
-            if(items.contains("\"kind\":")){
-                String kindItems = items.split("\"kind\":")[1].split("\"")[1];
-                jsonItems.put("kind",kindItems);
-            }
-            if(items.contains("\"etag\":")){
-                String etagItems = items.split("\"etag\":")[1].split("\"")[1];
-                jsonItems.put("etag",etagItems);
-            }
-            if(items.contains("\"id\":")){
-                String id = items.split("\"id\":")[1].split("\"")[1];
-                jsonItems.put("id",id);
-            }
-            if(items.contains("\"status\":")){
-                String status = items.split("\"status\":")[1].split("\"")[1];
-                jsonItems.put("status",status);
-            }
-            if(items.contains("\"htmlLink\":")){
-                String htmlLink = items.split("\"htmlLink\":")[1].split("\"")[1];
-                jsonItems.put("htmlLink",htmlLink);
-            }
-            if(items.contains("\"created\":")){
-                String created = items.split("\"created\":")[1].split("\"")[1];
-                jsonItems.put("created",created);
-            }
-            if(items.contains("\"updated\":")){
-                String updatedItems = items.split("\"updated\":")[1].split("\"")[1];
-                jsonItems.put("updated",updatedItems);
-            }
-            if(items.contains("\"summary\":")){
-                String summaryItems = items.split("\"summary\":")[1].split("\"")[1];
-                jsonItems.put("summary",summaryItems);
-            }
-            if(items.contains("\"description\":")){
-                String descriptionItems = items.split("\"description\":")[1].split("\"")[1];
-                jsonItems.put("description",descriptionItems);
-            }
-            if(items.contains("\"creator\":")){
-                String creator = items.split("\"creator\":")[1].split("\"")[1];
-                if(creator.contains("\"email\":")){
-                    String creatorEmail = creator.split("\"email\":")[1].split("\"")[1];
-                    jsonCreator.put("email",creatorEmail);
-                }
-                if(creator.contains("\"self\":")){
-                    String creatorSelf = creator.split("\"self\":")[1].split("\"")[1];
-                    jsonCreator.put("self",creatorSelf);
-                }
-                jsonItems.put("creator",jsonCreator);
-            }
-            if(items.contains("\"organizer\":")){
-                String organizer = items.split("\"organizer\":")[1].split("\"")[1];
-                if(organizer.contains("\"email\":")){
-                    String organizerEmail = organizer.split("\"email\":")[1].split("\"")[1];
-                    jsonOrganizer.put("email",organizerEmail);
-                }
-                if(organizer.contains("\"self\":")){
-                    String organizerSelf = organizer.split("\"self\":")[1].split("\"")[1];
-                    jsonOrganizer.put("self",organizerSelf);
-                }
-                jsonItems.put("organizer",jsonOrganizer);
-            }
-            if(items.contains("\"start\":")){
-                String start = items.split("\"start\":")[1].split("\"")[1];
-                if(start.contains("\"dateTime\":")){
-                    String startDateTime = start.split("\"dateTime\":")[1].split("\"")[1];
-                    jsonStart.put("dateTime", startDateTime);
-                }
-                if(start.contains("\"timeZone\":")){
-                    String startTimeZone = start.split("\"timeZone\":")[1].split("\"")[1];
-                    jsonStart.put("timeZone", startTimeZone);
-                }
-                jsonItems.put("start", jsonStart);
-            }
-            if(items.contains("\"end\":")){
-                String end = items.split("\"end\":")[1].split("\"")[1];
-                if(end.contains("\"dateTime\":")){
-                    String endDateTime = end.split("\"dateTime\":")[1].split("\"")[1];
-                    jsonEnd.put("dateTime", endDateTime);
-                }
-                if(end.contains("\"timeZone\":")){
-                    String endTimeZone = end.split("\"timeZone\":")[1].split("\"")[1];
-                    jsonEnd.put("timeZone", endTimeZone);
-                }
-                jsonItems.put("end", jsonEnd);
-            }
-            if(items.contains("\"iCalUID\":")){
-                String iCalUID = items.split("\"iCalUID\":")[1].split("\"")[1];
-                jsonItems.put("iCalUID",iCalUID);
-            }
-            if(items.contains("\"sequence\":")){
-                String sequence = items.split("\"sequence\":")[1].split("\"")[1];
-                jsonItems.put("sequence",sequence);
-            }
-            if(items.contains("\"reminders\":")){
-                String reminders = items.split("\"reminders\":")[1].split("\"")[1];
-                if(reminders.contains("\"useDefault\":")){
-                    String useDefault = reminders.split("\"useDefault\":")[1].split("\"")[1];
-                    jsonReminders.put("useDefault", useDefault);
-                }
-                jsonItems.put("reminders",jsonReminders);
-            }
-            if(items.contains("\"eventType\":")){
-                String eventType = items.split("\"eventType\":")[1].split("\"")[1];       
-                jsonItems.put("eventType",eventType);
-
-            }
-            jsonData.put("item", jsonItems);
-            
+        if(items.contains("\"summary\":")){
+            String summaryItems = items.split("\"summary\":")[1].split("\"")[1];
+            jsonItems.put("summary",summaryItems);
         }
+        if(items.contains("\"description\":")){
+            String descriptionItems = items.split("\"description\":")[1].split("\"")[1];
+            jsonItems.put("description",descriptionItems);
+        }
+        if(items.contains("\"creator\":")){
+            String creator = items.split("\"creator\":")[1];
+            if(creator.contains("\"email\":")){
+                String creatorEmail = creator.split("\"email\":")[1].split("\"")[1];
+                jsonCreator.put("email",creatorEmail);
+            }
+            if(creator.contains("\"self\":")){
+                String creatorSelf = creator.split("\"self\":")[1].split("\"")[1];
+                jsonCreator.put("self",creatorSelf);
+            }
+            jsonItems.put("creator",jsonCreator);
+        }
+        if(items.contains("\"organizer\":")){
+            String organizer = items.split("\"organizer\":")[1];
+            if(organizer.contains("\"email\":")){
+                String organizerEmail = organizer.split("\"email\":")[1].split("\"")[1];
+                jsonOrganizer.put("email",organizerEmail);
+            }
+            if(organizer.contains("\"self\":")){
+                String organizerSelf = organizer.split("\"self\":")[1].split("\"")[1];
+                jsonOrganizer.put("self",organizerSelf);
+            }
+            jsonItems.put("organizer",jsonOrganizer);
+        }
+        if(items.contains("\"start\":")){
+            String start = items.split("\"start\":")[1];
+            if(start.contains("\"dateTime\":")){
+                String startDateTime = start.split("\"dateTime\":")[1].split("\"")[1];
+                jsonStart.put("dateTime", startDateTime);
+            }
+            if(start.contains("\"timeZone\":")){
+                String startTimeZone = start.split("\"timeZone\":")[1].split("\"")[1];
+                jsonStart.put("timeZone", startTimeZone);
+            }
+            jsonItems.put("start", jsonStart);
+        }
+        if(items.contains("\"end\":")){
+            String end = items.split("\"end\":")[1];
+            if(end.contains("\"dateTime\":")){
+                String endDateTime = end.split("\"dateTime\":")[1].split("\"")[1];
+                jsonEnd.put("dateTime", endDateTime);
+            }
+            if(end.contains("\"timeZone\":")){
+                String endTimeZone = end.split("\"timeZone\":")[1].split("\"")[1];
+                jsonEnd.put("timeZone", endTimeZone);
+            }
+            jsonItems.put("end", jsonEnd);
+        }
+        if(items.contains("\"iCalUID\":")){
+            String iCalUID = items.split("\"iCalUID\":")[1].split("\"")[1];
+            jsonItems.put("iCalUID",iCalUID);
+        }
+        if(items.contains("\"sequence\":")){
+            String sequence = items.split("\"sequence\":")[1].split("\"")[1];
+            jsonItems.put("sequence",sequence);
+        }
+        if(items.contains("\"reminders\":")){
+            String reminders = items.split("\"reminders\":")[1];
+            if(reminders.contains("\"useDefault\":")){
+                String useDefault = reminders.split("\"useDefault\":")[1].split("\"")[1];
+                jsonReminders.put("useDefault", useDefault);
+            }
+            jsonItems.put("reminders",jsonReminders);
+        }
+        if(items.contains("\"eventType\":")){
+            String eventType = items.split("\"eventType\":")[1].split("\"")[1];       
+            jsonItems.put("eventType",eventType);
+        }
+        jsonData.put("item", jsonItems);
+    
         return jsonData;
     }
 
@@ -325,12 +313,19 @@ public class agendaGoogle {
 
     public static void main(String[] args) {
         // Example usage: Adding an event
-        String summary = "Meeting with Client";
+        String summary = "Meeting ";
         String description = "Discuss project progress and future plans.";
-        String startDateTime = "2024-03-23T17:00:00";
-        String endDateTime = "2024-03-23T19:00:00";
-        addEventToCalendar(summary, description, startDateTime, endDateTime);
+        String startDateTime = "2024-03-22T09:00:00";
+        String endDateTime = "2024-03-22T10:00:00";
+        //addEventToCalendar(summary, description, startDateTime, endDateTime);
 
-        // showUpcomingEvents(); do not work completly missing the parsing 
+        showUpcomingEvents(); //do not work completly missing the parsing 
+        
+        try{
+            System.out.println();
+            System.out.println(agendaGoogle.sendGetRequest(GOOGLE_CALENDAR_EVENTS_ENDPOINT));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
